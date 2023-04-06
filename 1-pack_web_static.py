bbe72287f87d068addf10890d01adda49c4508a2
Gatwan <abigailgathigia@gmail.com>
2023-04-06 16:42:55 +0300

    Fix error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,9 +9,9 @@ def do_pack():
     time = datetime.now()
     time_format = time.strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    archive_path = "versions/web_static_{}".format(time_format)
+    archive_path = "versions/web_static_{}.tgz".format(time_format)
     execute = local("tar -czvf {} web_static".format(archive_path))
-        
+
     if execute.failed:
         return None
     else:
