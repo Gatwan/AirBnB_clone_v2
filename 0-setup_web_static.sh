699ba39fe2ac1c784ceffdb94d6c343f349eb24a
Gatwan <abigailgathigia@gmail.com>
2023-04-06 16:14:52 +0300

    Fix error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,12 +4,12 @@ sudo apt -y update
 sudo apt -y install nginx
 sudo service nginx start
 
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo -e "Hey I Found You!" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '55i\\tlocation /hbnb_static/ {n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '55i\\tlocation /hbnb_static/ {n\t\talias /data/web_static/current/;\n\t\tindex index.html;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
