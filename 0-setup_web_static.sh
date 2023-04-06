7206950f0606a547c5e3b4774cf8fdd47dfa3b6d
Gatwan <abigailgathigia@gmail.com>
2023-04-06 12:38:20 +0300

    Add file 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#Sets up web servers for the deployment of web_static
+sudo apt -y update
+sudo apt -y install nginx
+sudo service nginx start
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Hey I Found You!" > /data/web_static/releases/test/index.html
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '95i\\tlocation /hbnb_static/ {n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo service nginx restart
