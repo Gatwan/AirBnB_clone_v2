d3d3f510f5acd3b64a33791ab8c050c50eac99c0
Gatwan <abigailgathigia@gmail.com>
2023-05-10 12:09:55 +0300

    [Fix]: Code error

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,31 +4,32 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """Function called with / route"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """Function called with /hbnb route"""
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_text(text):
     """Function called with /c/<text> route"""
     return 'C %s' % text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
 def python_text(text):
     """Function called with /python/(<text>) route"""
-    return 'Python %s' % text.replace('_', ' ')
+    return 'Python ' + text.replace('_', ' ')
 
 
 if __name__ == "__main__":
